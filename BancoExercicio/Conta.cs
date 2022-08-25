e5c0414e921944a37ec902d9d7054ca39207f8f6
Rafael Oliveira <rafaeloliveira.programador@gmail.com>
2022-08-25 01:03:09 -0300

    Add project files.

---
+++ BancoExercicio/Conta.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoExercicio
+{
+    internal class Conta
+    {
+        private int _numeroConta;
+        private string _nomeTitular;
+        private double _saldoConta = 0;
+
+        public Conta(int numeroConta, string nomeTitular, double depositoInicial = 0)
+        {
+            _numeroConta = numeroConta;
+            _nomeTitular = nomeTitular;
+
+            if (depositoInicial != 0)
+            {
+                this._saldoConta += depositoInicial;
+            } else
+            {
+                return;
+            }
+        }
+
+        public int NumeroConta
+        {
+            get { return _numeroConta; }
+            set { }
+
+        }
+        public string NomeTitular
+        {
+            get { return _nomeTitular; }
+            set { _nomeTitular = value; }
+        }
+        public double SaldoConta
+        {
+            get {return _saldoConta;}
+        }
+
+        // Os valores comentados abaixo, foram os antigos, pra poder utilizar o Console.WriteLine;
+
+        // public void Depositar(double valorDeposito)
+        public void Depositar()
+
+        {
+            double valorDeposito = 0;
+            Console.WriteLine("Digite o valor para deposito");
+            // valorDeposito = Double.Parse(Console.ReadLine());
+            valorDeposito = Double.Parse(Console.ReadLine());
+
+
+            this._saldoConta += valorDeposito;
+
+            Console.WriteLine(ToString());
+        }
+
+        // public void Sacar(double valorSaque)
+        public void Sacar()
+        {
+
+            Console.WriteLine("Digite o valor para realizar o saque");
+            // valorSaque = Double.Parse(Console.ReadLine());
+            double valorSaque = Double.Parse(Console.ReadLine());
+            this._saldoConta = (this._saldoConta - valorSaque) - 5.00;
+
+            Console.WriteLine(ToString());
+        }
+        public override string ToString()
+        {
+            return $"Dados da conta: \n Conta: {this.NumeroConta} Titular: {this.NomeTitular} Saldo: $ {this.SaldoConta.ToString("F2", CultureInfo.InvariantCulture)} ";
+        }
+    }
+
+}
---
+++ BancoExercicio/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BancoExercicio
+{
+    class Programa
+    {
+        public static void Main(string[] args)
+        {
+            double valorDeposito;
+
+            Console.WriteLine("********************--- SEJA BEM VINDO AO BANCO ELPODEROSO***************----");
+            Console.WriteLine("Digite o número que deseja para a Conta");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Muito bem, agora digite o seu nome completo\n");
+            string nomeCompleto = Console.ReadLine();
+            Console.WriteLine("Haverá deposito inicial? (S/N)");
+            char resposta = char.Parse(Console.ReadLine());
+            if(resposta == 's')
+            {
+                Console.WriteLine("Digite o valor que deseja depositar inicialmente");
+                valorDeposito = Double.Parse(Console.ReadLine());
+            }
+            else
+            {
+                valorDeposito = 0;
+            }
+
+            Conta c = new Conta(numeroConta, nomeCompleto, valorDeposito);
+            Console.WriteLine(c);
+
+            c.Depositar();
+            c.Sacar();
+
+            /*
+            Conta c = new Conta(2680, "Rafael Oliveira", 500);
+
+            c.Depositar(250);
+            Console.WriteLine(c.SaldoConta);
+            c.Sacar(250);
+            Console.WriteLine(c.SaldoConta);
+            */
+        }
+    }
+
+}
